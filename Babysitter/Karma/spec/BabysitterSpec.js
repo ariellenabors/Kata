e99744e30c4a8f941101ae358285012a08feae2e
Arielle Nabors <arielle.spring@gmail.com>
2016-01-11 21:36:09 -0500

    startTime and endTime function
    made test for startTime/endTime. wrote production code for
    startTime/endTime

--- Babysitter/Karma/spec/BabysitterSpec.js
+++ Babysitter/Karma/spec/BabysitterSpec.js
@@ -1,10 +1,21 @@
-describe("Babysitter", function() {
-  var babysitter = new Babysitter();
-  it("should have a babysitter class", function(){
-    expect(babysitter).toBeDefined();
-  });
+describe("Babysitter", function () {
+    var babysitter = new Babysitter();
+    it("should have a babysitter class", function () {
+        expect(babysitter).toBeDefined();
+    });
 
-  it("starts no earlier than 5:00PM", function() {
-  });
+    it("should return an error if start time is before 5:00PM", function () {
+        var startTime = 1600;
+        var errorMessage = 'too early';
+        expect(babysitter.calculatePayableHours(startTime)).toBe(errorMessage);
 
+    });
+
+    it("should return an error if end time is after 4:00AM", function () {
+        var endTime = 500;
+        var startTime = 1100;
+        var errorMessage = 'too late';
+        expect(babysitter.calculatePayableHours(startTime,endTime)).toBe(errorMessage);
+
+    });
 });
--- Babysitter/Karma/src/Babysitter.js
+++ Babysitter/Karma/src/Babysitter.js
@@ -1,3 +1,19 @@
 function Babysitter() {
 
+    this.calculatePayableHours=function(startTime,endTime){
+
+        var errorMessage = '';
+
+        if (startTime < 1700){
+            errorMessage = 'too early';
+        }
+
+        if (endTime > 400) {
+            errorMessage = 'too late';
+        }
+
+        return errorMessage;
+
+
+    }
 }
