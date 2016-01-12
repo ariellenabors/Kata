9b4106e62b79de7773ee7a04e37da78ef8cadad1
Arielle Nabors <arielle.spring@gmail.com>
2016-01-11 20:56:52 -0500

    defined babysitter
    created .toBeDefined for babysitter

---
+++ Babysitter/Karma/spec/BabysitterSpec.js
@@ -0,0 +1,10 @@
+describe("Babysitter", function() {
+  var babysitter = new Babysitter();
+  it("should have a babysitter class", function(){
+    expect(babysitter).toBeDefined();
+  });
+
+  it("starts no earlier than 5:00PM", function() {
+  });
+
+});
--- Babysitter/Karma/spec/PlayerSpec.js
+++
@@ -1,6 +0,0 @@
-describe("Babysitter", function() {
-
-  it("starts no earlier than 5:00PM", function() {
-  });
-
-});
