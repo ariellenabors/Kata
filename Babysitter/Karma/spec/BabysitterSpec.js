5446c9ebf4cb271631db81df4a73c0d8270f22d7
Arielle Nabors <arielle.spring@gmail.com>
2016-01-19 21:14:23 -0500

    wrote $8 and $16 per hour tests
    wrote tests for bed time to midnight payable hours and midnight to end
    of job payable hours.
    
    wrote production code for bed time to midnight payable hours and
    midnight to end of job payable hours.

--- Babysitter/Karma/spec/BabysitterSpec.js
+++ Babysitter/Karma/spec/BabysitterSpec.js
@@ -6,7 +6,7 @@ describe("Babysitter", function () {
 
     it("should return an error if start time is before 5:00PM", function () {
         var startTime = 1600;
-        var errorMessage = 'too early';
+        var errorMessage = 'start time is too early';
         expect(babysitter.calculatePayableHours(startTime)).toBe(errorMessage);
 
     });
@@ -14,7 +14,7 @@ describe("Babysitter", function () {
     it("should return an error if end time is after 4:00AM", function () {
         var endTime = 500;
         var startTime = 1100;
-        var errorMessage = 'too late';
+        var errorMessage = 'end time is too late';
         expect(babysitter.calculatePayableHours(startTime, endTime)).toBe(errorMessage);
 
     });
@@ -26,6 +26,21 @@ describe("Babysitter", function () {
             var endTime = 100;
             var expected = 48;
             expect(babysitter.calculatePayableHours(startTime, endTime, bedTime)).toBe(expected);
+        });
+
+        it('should be paid $8/hr from bedtime to midnight', function () {
+            var bedTime = 2200;
+            var midnight = 0;
+            var endTime = 100;
+            var expected = 16;
+            expect(babysitter.calculatePayableHours(bedTime, midnight, endTime)).toBe(expected);
+        })
+
+        it('should be paid $16/hr from midnight to end of job', function () {
+            var midnight = 0;
+            var endTime = 400;
+            var expected = 64;
+            expect(babysitter.calculatePayableHours(midnight, endTime)).toBe(expected);
         })
     })
 
--- Babysitter/Karma/src/Babysitter.js
+++ Babysitter/Karma/src/Babysitter.js
@@ -1,13 +1,24 @@
 function Babysitter() {
 
-    this.calculatePayableHours = function (startTime, endTime, bedTime) {
+    this.calculatePayableHours = function (startTime, endTime, bedTime, midnight) {
 
         var errorMessage = '';
+
         var moneyEarned = 0;
+        var moneyEarnedPartTwo = 0;
+        var moneyEarnedPartThree = 0;
+
         var startTimeToBedTimeRate = .12;
+        var bedTimeToMidnightRate = .08;
+        var midnightToEndTimeRate = .16;
+
         var hoursWorkedFromStartToBed = bedTime - startTime;
+        var hoursWorkedFromBedToMidnight = midnight - bedTime;
+        var hoursWorkedFromMidnightToEndTime = endTime - midnight;
 
         moneyEarned = startTimeToBedTimeRate * hoursWorkedFromStartToBed;
+        moneyEarnedPartTwo = bedTimeToMidnightRate * hoursWorkedFromBedToMidnight;
+        moneyEarnedPartThree = midnightToEndTimeRate * hoursWorkedFromMidnightToEndTime;
 
         //reset moneyEarned summation, calculate bed time to midnight, midnight to end of job
         //bed time to midnight rate; midnight to end time rate
@@ -16,16 +27,24 @@ function Babysitter() {
             return moneyEarned;
         }
 
+        if (moneyEarnedPartTwo > 0){
+            return moneyEarnedPartTwo;
+        }
+
+        if (moneyEarnedPartThree > 0){
+            return moneyEarnedPartThree;
+        }
+
         return determineCorrectStartEndTime(startTime, endTime);
     };
 
     function determineCorrectStartEndTime(startTime, endTime) {
         if (startTime < 1700) {
-            errorMessage = 'too early';
+            errorMessage = 'start time is too early';
         }
 
         if (endTime > 400) {
-            errorMessage = 'too late';
+            errorMessage = 'end time is too late';
         }
 
         return errorMessage;
